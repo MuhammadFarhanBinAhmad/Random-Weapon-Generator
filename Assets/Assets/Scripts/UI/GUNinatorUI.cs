46d1ef6fc52b27cd45e21ced8c50758aa0dd4449
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2021-07-18 15:41:32 +0800

    UI Guniator

--- Assets/Assets/Scripts/UI/GUNinatorUI.cs
+++ Assets/Assets/Scripts/UI/GUNinatorUI.cs
@@ -8,6 +8,12 @@ public class GUNinatorUI : MonoBehaviour
     GUNINATORGunCreation the_GGC;
     public TextMeshProUGUI total_Weapon_Cost;
 
+    [Header("Weapon Lock Buttons")]
+    public List<bool> weapon_Unlock = new List<bool>();
+    public List<bool> rarity_Unlock = new List<bool>();
+    public List<bool> round_Unlock = new List<bool>();
+    public List<bool> element_Unlock = new List<bool>();
+
     private void Start()
     {
         the_GGC = GetComponent<GUNINATORGunCreation>();
@@ -16,5 +22,9 @@ public class GUNinatorUI : MonoBehaviour
     public void UpdateWeaponCost()
     {
         total_Weapon_Cost.text = "Total Cost: " + the_GGC.w_Total_Cost.ToString();
+    }
+    public void UpdateLockItems()
+    {
+
     }
 }
